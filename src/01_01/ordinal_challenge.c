316220b43580e28ae077ba5912975c01d577b7e0
joslynk87 <120771529+joslynk87@users.noreply.github.com>
2022-12-18 17:54:03 +0000

    ordinal challenge

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -6,7 +6,7 @@ char *ordinal(int v)
 	   - create this function
 	   - this code won't run with out it!
 	 */
-	if ( v == 11 | v == 12 || v == 13 ) {
+		if ( v == 11 | v == 12 || v == 13 ) {
 		return "th";
 	}
 
@@ -21,6 +21,7 @@ char *ordinal(int v)
 	}
 
 	return "th";
+
 }
 
 int main()
